e729c2f4b2c58e59ff2e7feab8ad1ea7ec70086f
bleartdemaku01 <bdemaku15@gmail.com>
2023-11-30 14:21:12 +0100

    hello

--- src/constants/index.js
+++ src/constants/index.js
@@ -175,7 +175,7 @@ export const projects = [
   {
     iconUrl: threads,
     theme: "btn-back-green",
-    name: "Banking Website",
+    name: "Banking Front-End Website",
     description:
       'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
     link: "https://github.com/bleartdemaku/bank/tree/main/bank",
